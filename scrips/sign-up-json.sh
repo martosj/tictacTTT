163bf8608710ab2741033d3d8257e319ebd94516
martosj <martosj@yahoo.com>
2017-07-16 11:37:00 -0400

    Signupjson.sh commited

---
+++ scrips/sign-up-json.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+curl "https://ga-library-api.herokuapp.com/sign-up" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --data '{
+    "credentials": {
+      "email": "'"${EMAIL}"'",
+      "password": "'"${PASSWORD}"'",
+      "password_confirmation": "'"${PASSWORD_CONFIRMATION}"'"
+    }
+  }'
+
+# data output from curl doesn't have a trailing newline
+echo
